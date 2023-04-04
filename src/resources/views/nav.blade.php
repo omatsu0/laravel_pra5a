7ef9b86f63a79c686bde8d4bf4af662596c9fc63
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2023-04-04 14:04:46 +0900

    投稿画面作成

--- src/resources/views/nav.blade.php
+++ src/resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
 
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
 
   <ul class="navbar-nav ml-auto">
 
@@ -15,10 +15,10 @@
       <a class="nav-link" href="{{ route('login') }}">ログイン</a>
     </li>
     @endguest
-      
+
     @auth
     <li class="nav-item">
-      <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+      <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
     @endauth
     
