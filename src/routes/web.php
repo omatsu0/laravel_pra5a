ec0b0cddd798a0ba95373b89496541be1a5732f6
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2023-04-05 16:12:32 +0900

    記事詳細ページ追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,5 +13,6 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); 
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']); 
 
