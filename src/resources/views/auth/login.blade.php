8a2938e88166ba7ddeb7046bfd4eecce0e1a492f
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2023-04-06 15:40:10 +0900

    パスワード再送信画面作成

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -30,7 +30,11 @@
                 {{--ここから--}}
                 <input type="hidden" name="remember" id="remember" value="on">
                 {{--ここまで--}}
-
+                
+                <div class="text-left">
+                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+                
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
